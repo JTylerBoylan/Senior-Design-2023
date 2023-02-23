5ca4dd79b54ddbc149d9c173014ad942319792bf
JTylerBoylan <jtylerboylan@outlook.com>
2023-02-23 13:12:50 -0500

    PWM?

--- sd504_motor_controller/sd504_motor_controller/motor_controller_node.py
+++ sd504_motor_controller/sd504_motor_controller/motor_controller_node.py
@@ -12,18 +12,25 @@ class MotorControllerNode(Node):
     def __init__(self):
         super().__init__('motor_controller_node')
         self.get_logger().info('Motor Controller Initialized.')
+        while(True):
+            GPIO.output(output_pin_A1, GPIO.HIGH)
+            time.sleep(1/100_000)
+            GPIO.output(output_pin_A1, GPIO.LOW)
+            time.sleep(1/100_000)
+        
         
 def main(args=None):
     # Initialize Node
     rclpy.init(args=args)
     # Initialize GPIO
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(output_pin_A1, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(output_pin_A1, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setwarnings(False)
     # Initialize Motor Controller
     motor_controller_node = MotorControllerNode()
     # Spin node
     rclpy.spin(motor_controller_node)
+    print('Shutting down')
     # Destroy when finished
     motor_controller_node.destroy_node()
     rclpy.shutdown()
