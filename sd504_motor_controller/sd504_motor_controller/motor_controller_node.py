1c2caaf7173fc70a676b45c1211580ede260a6f8
Jonathan Tyler Boylan <63868524+JTylerBoylan@users.noreply.github.com>
2023-03-21 19:38:37 -0400

    Update motor_controller_node.py

--- sd504_motor_controller/sd504_motor_controller/motor_controller_node.py
+++ sd504_motor_controller/sd504_motor_controller/motor_controller_node.py
@@ -25,9 +25,10 @@ class MotorControllerNode(Node):
             self.drive_callback,
             10
         )
-
-        #test
-        self.serial_port.write(52)
+        
+        # Set up communicate loop
+        self.comm_frequency = 50 # Hz
+        self.comm_timer = self.create_timer(self.comm_frquency, self.comm_callback)
 
         self.get_logger().info('Motor Controller Initialized.')
 
@@ -36,7 +37,22 @@ class MotorControllerNode(Node):
 
     def drive_callback(self, msg):
         self.drive_power = msg;
-    
+        
+    def comm_callback(self):
+        # Send motor signals to Teensy
+        self.serial_port.write(str(self.drive_power).encode())
+        self.serial_port.write(str(self.steering_angle).encode())
+        self.serial_port.write(str(0).encode())
+        self.serial_port.write(str(0).encode())
+        
+        # Receive steering encoder value from Teensy
+        # data = self.serial_port.readline().decode()
+        # publish data to /motors/encoder
+        # self.get_logger().info('Published %d to /motors/encoder', data)
+        
+    def __del__(self):
+        # close serial port when node is destroyed
+        self.serial_port.close()
         
 def main(args=None):
     # Initialize Node
